0601e435ec790e3a36430f3f7c9448aa3d1b981b
Rebecca Keegan <rebeccachristinekeegan@gmail.com>
2019-04-24 15:11:04 +0100

    working quotes page

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log("hello world!");
+
+const express = require('express')
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect('mongodb+srv://rebecca-keegan:password1234@mondodb-express-azvex.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('mondodb-express') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      // renders index.ejs
+      res.render('index.ejs', {quotes: result})
+    })
+  })
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').insertOne(req.body, (err, result) => {
+      if (err) return console.log(err)
+  
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
+
+app.set('view engine', 'ejs')
+
